807713dfeae26323d2f70d9a558b0b6955bd407e
saraalvarex <alvarezalmacen19@gmail.com>
2022-11-08 14:23:23 +0100

    commit

--- src/components/DeleteDepartamento.vue
+++ src/components/DeleteDepartamento.vue
@@ -1,7 +1,23 @@
 <template>
- <div>
-    <h5 style="color: red">Eliminar</h5>
- </div>
+<div id="modal" class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -16,11 +32,16 @@ export default {
     };
  },
  mounted(){
-    var id = this.$route.params.id;
-    service.deleteDepartamento(id).then(result=> {
+    
+ },
+ methods: {
+    deleteDepartamento(){
+        var id = this.$route.params.id;
+        service.deleteDepartamento(id).then(result=> {
         console.log(result);
         this.$router.push("/");
     })
+    }
  }
 };
 </script>
